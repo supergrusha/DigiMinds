2657e4a022b1236988722ecd12767036e8f2b0cc
Dimitri <dhrusha92@gmail.com>
2014-01-29 20:48:01 +0100

    Cake Site added to repository

---
+++ CakeSite/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+$(".col-md-4 .arrow.img-responsive.center-block").on({
+ "mouseover" : function() {
+    this.src = 'img/cake-buttonhover.jpg';
+  },
+  "mouseout" : function() {
+    this.src='img/cake-button.jpg';
+  }
+});
+
+});
