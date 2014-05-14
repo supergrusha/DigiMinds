0a219bd2adcf4f2d80c4869c033c9784a1a4811b
Dimitri <dhrusha92@gmail.com>
2014-05-14 21:08:00 +0200

    @Slider added

--- CakeSite/js/script.js
+++ CakeSite/js/script.js
@@ -10,4 +10,19 @@ $(".col-md-4 .arrow.img-responsive.center-block").on({
   }
 });
 
+var slider = $('ul.slider'); 
+var effect = 'scrollHorz';
+
+
+	if(slider.length > 0) {
+		$('ul.slider').cycle({ 
+			timeout:  6000,
+			fx:     effect,
+			pager: '.bannernav',
+			next: '.arrow-right',
+			prev: '.arrow-left'			
+		});
+}
+
+
 });
